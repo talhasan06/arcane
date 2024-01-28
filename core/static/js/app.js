7fecc3691ec2e3c7431039aef0d6072bd74e6568
Tamim Al Hasan <talhasan06@gmail.com>
2024-01-28 20:55:35 +0600

    review,login,register style

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -11,3 +11,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
+
+$(document).ready(function(){
+     $(".owl-carousel").owlCarousel({
+          loop:true,
+          margin:10,
+          nav:true,
+          responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:3
+              },
+              1000:{
+                  items:5
+              }
+          }
+     })
+});
